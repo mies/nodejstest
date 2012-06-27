d97c3969eb4be796ab5543520f9c569db3744e41
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-06-27 10:05:34 -0700

    first commit

---
+++ helloworld/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.configure(function() {
+  'use strict';
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+app.get('/', function(req, res) {
+  'use strict';
+  res.send("<h1 style='font-size: 500%;'>Hello World!</h1>");
+});
+
+app.get('/json', function(req, res) {
+  'use strict';
+  res.send({'hello' : 'all!'});
+});
+
+var port = process.env.PORT || 3000 ;
+
+app.listen(port, function() {
+  'use strict';
+  console.log('listening on port ' + port);
+});
