befb5471cfff0e0719371cfe85ab9ec85c6131df
wouterm <hebrox@gmail.com>
2012-06-27 18:11:22 -0700

    dusss

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var app = express.createServer();
-
-app.get('/', function(req, res) {
+require('http').createServer(function (request, response) {
   'use strict';
-  res.send("<h1 style='font-size: 500%;'>Hello World!</h1>");
-});
-
-app.listen(8080);
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  var k;
+  var output = "Hello World`Domination!\n";
+  for (k in request.headers) {
+    output += k + '=' + request.headers[k] + '\n';
+  }
+  response.end(output);
+}).listen(8080);
