59129ccda0c6bd4eab6f2518b128c87f7a976462
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-06-27 17:37:03 -0700

    removed code

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,25 +1,10 @@
 var express = require('express');
-var app = express.createServer(express.logger());
-
-app.configure(function() {
-  'use strict';
-  app.use(express.bodyParser());
-  app.use(app.router);
-});
+var app = express.createServer();
 
 app.get('/', function(req, res) {
   'use strict';
   res.send("<h1 style='font-size: 500%;'>Hello World!</h1>");
 });
 
-app.get('/json', function(req, res) {
-  'use strict';
-  res.send({'hello' : 'all!'});
-});
-
-//var port = process.env.PORT || 8080 ;
-
 app.listen(8080, function() {
-  'use strict';
-  console.log('listening on port 8080');
 });
